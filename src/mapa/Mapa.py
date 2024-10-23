7714dd9e23ab97cf5c018ae93df53f5197fbdad5
krisMG21 <cristimg9@gmail.com>
2024-10-23 16:07:45 +0200

    md del repo de antlr para python

--- src/mapa/Mapa.py
+++ src/mapa/Mapa.py
@@ -2,12 +2,12 @@ from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
 from antlr4.ParserRuleContext import ParseTree
 from MapaTesoroParser import MapaTesoroParser
 from MapaTesoroLexer import MapaTesoroLexer
-from mapaListener import MapaListener
+from MapaListener import MapaListener
 from TreePrinter import TreePrinter
 
 
 class Mapa:
-    def __init__(self, text, min_size=0):
+    def __init__(self, text: str, min_size: int = 0):
         self.min_size = min_size
         self.titulo, self.barcos, self.size, self.tree = self.parse_map(text)
 
@@ -21,10 +21,10 @@ class Mapa:
         for barco in self.barcos:
             self.mapa[barco.coordenadas[0] - 1][barco.coordenadas[1] - 1] = barco
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int):
         return self.mapa[key]
 
-    def __contains__(self, key):
+    def __contains__(self, key: int):
         return key in self.mapa
 
     def get_barcos(self):
@@ -44,7 +44,7 @@ class Mapa:
         walker = ParseTreeWalker()
         walker.walk(printer, self.tree)
 
-    def parse_map(self, text):
+    def parse_map(self, text: str):
         lexer: MapaTesoroLexer = MapaTesoroLexer(InputStream(text))
         tokens: CommonTokenStream = CommonTokenStream(lexer)
         parser: MapaTesoroParser = MapaTesoroParser(tokens)
